7ae27ab49d9042fc430941b51bcb4f4f6f15bafc
anderson.holguin <anderson.holguin@umb.edu.co>
2013-11-16 12:09:12 -0500

    estructura de gurdar datos

--- view/datos/datos.js
+++ view/datos/datos.js
@@ -1,11 +1,35 @@
+function validar() {
+    // se deshabilita acordion para evitar conflicto de validación
+//    if (respuesta)
+//        //crearAjaxForm();
+//        return respuesta;
+}
+
+
 $(document).ready(function() {
-    $('#botonGuardarDatos').click(function() {
-        $("#contenido").load(
-                'direc.php', {
-            'url': 'controller/avatar/avatar.php',
-            'content': 'false'
-        });
+
+    $('#formularioDatos').ajaxForm({
+        complete: function(xhr) {
+            //si guarda el registro se actualiza el listado de  estudiantes
+            //
+            if (xhr.responseText == '1') {
+                alert('llega');
+            }
+            else {
+                alert('no llega');
+
+            }
+
+            $('#contenido').html(xhr.responseText);
+        }
     });
+//    $('#botonGuardarDatos').click(function() {
+//        $("#contenido").load(
+//                'direc.php', {
+//            'url': 'controller/avatar/avatar.php',
+//            'content': 'false'
+//        });
+//    });
 });
 
 
