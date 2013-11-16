6db7ef63a7598320e66b6ef2716fb7b7df04b148
anderson.holguin <anderson.holguin@umb.edu.co>
2013-11-16 12:26:15 -0500

    tmp datos para poder hacer merge

--- view/datos/datos.php
+++ view/datos/datos.php
@@ -21,23 +21,23 @@
     <div id="aviso-2"></div>
 
     <div id="aform">
-        <form class="formulario-1" action="direc.php" id="formularioDatos" method="post" onSubmit = "return validar(this)">
-            <input type="hidden" value="controller/datos/saveDatos.php" name="url"/>
-            <input type="hidden" value="false" name="content"/>
+
+        <form class="formulario-1" id="" action="" method="" onsubmit="">
+
             <div>
                 <label>Nombre</label>
-                <input name="nombre" class="campo" type="text" value=""  /> 
+                <input class="campo" type="text" value=""  /> 
             </div>
 
             <div>
                 <label>Identificación</label>
-                <input name="identificacion" class="campo" type="text" value=""  /> 
+                <input class="campo" type="text" value=""  /> 
             </div>
 
 
             <div>  
                 <label> Ocupación actual</label>
-                <select class="lista-1" name="ocupacion_id">
+                <select class="lista-1" name="ocupacion">
                     <?PHP
                     foreach ($ocupaciones as $key => $ocupacion) {
                         echo "<option value='{$ocupacion['id']}]'>{$ocupacion['nombre']}</option>";
@@ -49,7 +49,7 @@
 
             <div>
                 <label>Nivel educativo de la madre</label>
-                <select class="lista-1" name="nivel_estudio_id">
+                <select class="lista-1" name="lista-uno">
                     <?PHP
                     foreach ($niveles as $key => $nivel) {
                         echo "<option value='{$nivel['id']}]'>{$nivel['nombre']}</option>";
@@ -60,31 +60,31 @@
 
             <div>  
                 <label>Qué aficiones tiene</label>
-                <textarea class="text-area1" name="aficiones"></textarea> 
+                <textarea class="text-area1"></textarea> 
             </div> 
 
             <div>  
                 <label>Cómo ocupa su tiempo libre</label>
-                <textarea class="text-area1" name="tiempo_libre"></textarea>
+                <textarea class="text-area1"></textarea>
             </div> 
 
             <div>
                 <label>Práctica algún deporte</label>
-                <span>Si</span><input class="campo-2" name="practica_deporte" type="radio" value="1"/>
-                <span>No</span><input class="campo-2" name="practica_deporte" type="radio" value="0" />
-                <span>Cuales:</span><textarea class="text-area1" name="Deportes"></textarea>
+                <span>Si</span><input class="campo-2" name="deporte" type="radio" />
+                <span>No</span><input class="campo-2" name="deporte" type="radio" />
+                <span>Cuales:</span><textarea class="text-area1"></textarea>
             </div> 
 
             <div>      
                 <label>Tiene hijos</label>
-                <span>Si</span><input type="radio" name="hijos" value="1" />
-                <span>No</span><input type="radio" name="hijos" value="0" />
-                <span>Cuantos:</span><textarea class="text-area1" name="n_hijos"></textarea> 
+                <span>Si</span><input type="radio" name="hijos" />
+                <span>No</span><input type="radio" name="hijos" />
+                <span>Cuantos:</span><textarea class="text-area1"></textarea> 
             </div>
 
             <div>  
                 <label>Estado Civil</label>  
-                <select class="lista-1" name="estado_civil_id">
+                <select class="lista-1" name="lista-tres">
                     <?PHP
                     foreach ($estados as $key => $estado) {
                         echo "<option value='{$estado['id']}]'>{$estado['nombre']}</option>";
@@ -95,18 +95,18 @@
 
             <div>  
                 <label>Sexo</label>  
-                <select class="lista-1" name="genero">
-                    <option value="M">Masculino</option>
-                    <option value="F">Femenino</option>
+                <select class="lista-1" name="lista-tres">
+                    <option value="">Masculino</option>
+                    <option value="">Femenino</option>
                 </select>
             </div>    
 
             <div class="clear"></div>
-            <input type="submit" id="botonGuardarDatos" value="Guardar Información" id="botonGuardar">
-        </form>
+            <a href="#" id="botonGuardarDatos">Guardar</a>
+    </form>
 
 
-    </div>
+</div>
 
 
 
