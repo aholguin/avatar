9dc27cb196cafdf7c747c548bf001c6ad3bfc01e
diego.castro <umbumb@dir_financiera.umb.local>
2013-11-21 18:39:40 -0500

    escenario cambio

--- view/avatar/avatar.php
+++ view/avatar/avatar.php
@@ -5,11 +5,15 @@
  */
 ?>
 <script type="text/javascript" src="view/avatar/avatar.js"></script>
-<div class="grid_12" align="center">
+<link rel="stylesheet" type="text/css" href="view/avatar/avatar.css" />
+
+<div class="grid_12"  align="center" id="slider-mod">
     <ul class="rs-slider">
         <li><img src="view/avatar/imagenes/person-1.gif" alt="" /></li>
         <li><img src="view/avatar/imagenes/person-2.gif" alt="" /></li>
         <li><img src="view/avatar/imagenes/person-3.gif" alt="" /></li>
     </ul>
+    
+    <div class="bund"></div>
     <a href="#" id="botonSelectAvatar">Seleccionar</a>
 </div>
