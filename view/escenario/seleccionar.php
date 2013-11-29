df3d6b26f756dd54c2136e85b77500fd0a7e470a
diego.castro <umbumb@mac-pro-de-umb-umb.local>
2013-11-28 21:17:28 -0500

    cambiosNoche

--- view/escenario/seleccionar.php
+++ view/escenario/seleccionar.php
@@ -20,5 +20,27 @@
     <div class="btn-b" id="botonEscenario2"></div>
     <div class="btn-c" id="botonEscenario3"></div>
     <div class="btn-d" id="botonEscenario4"></div>
+    
+   <div class="avatara">
+       <div class="ima"></div>
+       <div class="txt"><p></p></div>
+    </div>
+    
+    <div class="avatarb">
+       <div class="ima2"></div>
+       <div class="txt2"><p></p></div>
+    </div>
+    
+    <div class="avatarc">
+       <div class="ima3"></div>
+       <div class="txt3"><p></p></div>
+    </div>
+    
+    <div class="avatard">
+       <div class="ima4"></div>
+       <div class="txt4"><p></p></div>
+    </div>
+
+    
 
 </div>
